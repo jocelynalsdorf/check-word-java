2af64cedcf3433b9f2f6a3f0fa1c4be6bde09c31
jocelynalsdorf <jfrodsla@gmail.com>
2015-08-14 09:46:48 -0700

    adds web page functionality and adjusts output on forms; routes working as expected

--- src/main/java/CheckWord.java
+++ src/main/java/CheckWord.java
@@ -9,28 +9,28 @@ import static spark.Spark.*;
 
 public class CheckWord {
   public static void main(String[] args) {
-    // staticFileLocation("/public");
-    // String layout = "templates/layout.vtl";
-    // get("/", (request, response) -> {
-    //   Map<String, Object> model = new HashMap<String, Object>();
-    //   model.put("template", "templates/home.vtl");
-    //   return new ModelAndView(model, layout);
-    // }, new VelocityTemplateEngine());
+    staticFileLocation("/public");
+    String layout = "templates/layout.vtl";
+    get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/home.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
     
-    // get("/results", (request, response) -> {
-    //   Map<String, Object> model = new HashMap<String, Object>();
-    //   model.put("template", "templates/results.vtl");
+    get("/results", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/results.vtl");
     
-    //   String something = request.queryParams("something");
-    //  
+      String sentence = request.queryParams("sentence");
+     
     
-    //   Integer getMethodName = getMethodName(something, somethingElse);
+      String getChanged = getChanged(sentence);
     
-    //   model.put("something", something);
-    //  
-    //   model.put("getMethodName", getMethodName(something, somethingElse));
-    //   return new ModelAndView(model, layout);
-    // }, new VelocityTemplateEngine());
+      model.put("sentence", sentence);
+     
+      model.put("getChanged", getChanged(sentence));
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
 
   }
   public static String getChanged(String sentence){
